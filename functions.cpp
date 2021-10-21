1e4289cdb6591b877acc319ce6e8a4111718db87
Guolinang <aadyulkeeva@edu.tversu.ru>
2021-10-21 22:02:50 +0300

    +

---
+++ functions.cpp
@@ -0,0 +1,231 @@
+#include <stdio.h>
+#include <math.h> 
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+
+#include "functions.h"
+
+double EX_1(int x, int a, int c)
+{
+   long double L = 0;
+   long double L1= 0;
+
+   L = cos(pow(x, 2) * pow(a, 5));
+   L = pow(L, 4);
+   L = sqrt(exp(x) - L);
+   L1 = atan(a - pow(x, 4));
+   L1 = pow(L1, 4);
+   L = L + L1;
+   c = pow(c, 4);
+   L1 = exp(1)*sqrt(abs(a + x * c));
+   L = L / L1;
+   return (L);
+}
+
+long int EX_2(int t)
+{
+   int u = 0;
+   u = (3 * t * t) - (6 * t);
+   return (u);
+}
+
+void EX_3()
+{
+   float x1 = 0, x2 = 0;
+   printf("Введите значения a,b,c ");
+   int a = 0, b = 0, c = 0;
+   scanf_s("%d %d %d", &a, &b, &c);
+   printf("%dx^2+%dx+%d=0\n", a, b, c);
+   float D = 0;
+   D = b * b - 4 * a * c;
+   if (D < 0)
+   {
+      
+      printf("Корней нет\n");
+   }
+   if (D == 0)
+   {
+      x1 = (-b) / (2 * a);
+      printf("Корень один и равен он %d \n", x1);
+   }
+   if (D > 0)
+   {
+            x1 = ((-b) - sqrt(D))/ (2*a);
+      x2 = ((-b) + sqrt(D)) / (2*a);
+      printf("Кореня два: %d и %d \n", x1, x2);
+   }
+   return;
+}
+
+unsigned int EX_4(double time, int code)
+{
+   int price = 0;
+   switch (code)
+   {
+   case 48 :
+      price = time * 15;
+      break;
+   case 44:
+      price = time * 18;
+      break;
+   case 46:
+      price = time * 13;
+      break;
+   case 45:
+      price = time * 11;
+      break;
+   default:
+      printf("введено неверный код города");
+   }
+   return price;
+}
+
+void EX_5( )
+{
+   for (int i = 1000 ; i < 10000; ++i)
+   {
+      int ssum = 0;
+      int i1 = i;
+      int n = 0;
+      while (i1 > 0)
+      {
+         n = i1 % 10;
+         ssum = ssum + pow(n, 4);
+         i1 = i1 / 10;
+      }
+      if (ssum == i)
+      {
+         printf("%d\n", i);
+        
+      }
+   }
+   return;
+}
+
+
+
+///Перевод из десятичной в двоичную
+void EX_65()
+{
+   int numb = 0;
+   printf("Введите число\n");
+   scanf_s("%d",&numb );
+   int kolvo = 0;
+   int znack = ceil(log2(numb))+1;
+   int *a;
+   a = (int*)malloc(znack * sizeof(int));
+   for (int i = 0; i < znack; ++i)
+   {
+      a[i] = numb % 2;
+      numb = numb / 2;
+   }
+   for (int i = znack-1; i >=0; --i)
+      printf("%d", a[i]);
+   return;
+
+
+}
+
+
+//Перевод из двоичной в десятичную
+void EX_6()
+{
+   int n = 0;
+   printf("Введите размер массива");
+   scanf_s("%d", &n);
+   int* a;
+   int newnumb = 0;
+   a = (int*)malloc(n * sizeof(int));
+   for (int i = 0; i < n; ++i)
+   {
+      scanf_s("%d", &a[i]);
+      newnumb= a[i] + newnumb*2 ;
+   }
+   printf("%d",newnumb);
+
+
+
+
+}
+
+/*void EX_7()
+{
+   int I = 0;
+   int J = 0;
+
+   printf("Введите размер массива");
+
+   scanf_s("%d%d", &I, &J);
+
+   int** L;
+
+   L = (int**)malloc(I * J * sizeof(int*));
+
+   srand(time(NULL));
+
+
+   for (int l = 0; l < I * J; l++)
+   {
+      L[l] = rand() % 21 - 10;
+      printf("%4d ", L[l]);
+
+   }
+
+   for (int i = 0; i < I; i++)
+   {
+      for (int j = 0; j < J; j++)
+      {
+         L[i][j] = L[i][j] * 3;
+         if (L[i][j] > 0)
+            printf("-%4d ", (L[i][j]));
+         if (L[i][j] < 0)
+            printf("%4d ", (L[i][j]));
+      }
+   }
+
+*/
+
+
+
+void EX_7()
+{
+   int I = 0;
+   int J = 0;
+
+   printf("Введите размер массива");
+
+   scanf_s("%d%d", &I, &J);
+
+   srand(time(NULL));
+
+   int* L =(int*)malloc(I * J * sizeof(int*));
+
+   for (int l = 0; l < I * J; l++)
+   {
+      L[l] = rand() % 21 - 10;
+   }
+   for (int i = 0; i < I; i++)
+   {
+      for (int j = 0; j < J; j++)
+      {
+         printf("%d ", L[i * J + j]);
+      }
+      printf("\n");
+   }
+
+   printf("\n");
+
+   for (int i = 0; i < I; i++)
+   {
+      for (int j = 0; j < J; j++)
+      {
+         L[i * J + j] = 3 * (-L[i * J + j]);
+         printf("%d ", L[i * J + j]);
+      }
+      printf("\n");
+   }
+
+   return;
+}
+
---
+++ functions.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <stdbool.h>
+
+
+/*
+* Первое задание
+* @param x,a,c - вводимые с клавиатуры значения
+* @return - значение функции
+*/
+double EX_1(int x, int a, int c);
+
+
+/*
+* Второе задание
+* @param t - значение времени
+* @return - значение скорости тела в момент времени t
+*/
+long int EX_2(int t);
+
+
+/*
+* Третье задание
+* @param a,b,c - значения констатант 
+* @return - значение неизвестной
+*/
+void EX_3 ( );
+
+
+/*
+* Четвертое задание
+* @param time, code - время и код города
+* @return - цена разговора
+*/
+unsigned int EX_4(double time, int code);
+
+
+/*
+* Пятое задание
+* @return - Вывод чисел Армстронга
+*/
+void EX_5 ( );
+
+
+void EX_65();
+
+void EX_6();
+
+void EX_7();
